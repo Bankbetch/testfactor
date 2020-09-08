7b42c7c943383267326ef05148db1541ef7becea
Bankbetch <jiraphun.jrb@gmail.com>
2020-09-08 22:37:08 +0700

    add file cloudbuild

---
+++ cloud_bundle_install.sh
@@ -0,0 +1,8 @@
+eval $(ssh-agent -s)
+ssh-add <(echo "{$_SSH_PRIVATE_KEY}")
+echo "{$_SSH_PRIVATE_KEY}" | tr -d '\r' | ssh-add - >/dev/null
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
+ssh-keyscan {$_PROD_SERVER} >>~/.ssh/known_hosts
+chmod 644 ~/.ssh/known_hosts
+'[[ -f /.dockerenv ]] && echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config'
